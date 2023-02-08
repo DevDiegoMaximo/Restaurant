0f774873f7b41d1003edda782167ba7275ba3e92
Dimaximo <d.maximoalbuquerque@gmail.com>
2023-02-08 18:15:13 -0300

    final project done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,11 @@ import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent, CartPageComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from '../services/cart/cart.service';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/food';
 
@@ -12,11 +13,18 @@ export class FoodPageComponent implements OnInit {
   food!: Food;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private foodService: FoodService
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
   ) {
     activatedRoute.params.subscribe((params) => {
       if (params['id']) this.food = foodService.getFoodById(params['id']);
     });
   }
   ngOnInit(): void {}
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
